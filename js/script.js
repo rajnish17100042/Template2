99302d951c429bba1593ff8cf079308c27a7441f
Rajnish <rajnish17100042@gmail.com>
2021-07-27 22:45:58 +0530

    added js navbar feature

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ AOS.init({
     mirror: false,
 
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
